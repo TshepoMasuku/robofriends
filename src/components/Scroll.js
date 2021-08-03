9c890a6697ff7a2d11ba4b99051c34f2e9cae6af
Tshepo Masuku <tshepomasuku16@gmail.com>
2021-08-03 18:10:17 +0200

    unknown commit.

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return(
-         <div style={{overflowY: 'scroll', height:'66vh'}}>
+        <div style={{overflowY: 'scroll', height:'66vh'}}>
             {props.children}
         </div>
     )
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,6 @@ import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
-// This means the current directory './', and this means the previous directory '../'.
 
 class App extends React.Component {
   constructor() {
@@ -18,14 +17,10 @@ class App extends React.Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then( response => response.json() )
-    .then( users => this.setState({ robots: users }) )
+      .then( response => response.json() )
+      .then( users => this.setState({ robots: users }) )
   }
 
-  // onSearchChange = () => {
-  //   const input = document.querySelector('input');
-  //   this.setState( { searchfield: input.value } )
-  // }
   onSearchChange = (event) => {
     this.setState( { searchfield: event.target.value } )
   }
@@ -44,7 +39,6 @@ class App extends React.Component {
           <h1 className='f1'>ROBOFRIENDS</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <br></br>
-          {/* <hr></hr> */}
           <Scroll>
             <br></br>
             <ErrorBoundry>
